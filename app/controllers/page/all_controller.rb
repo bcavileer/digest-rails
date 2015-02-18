6f131b304d7d32b67a00cee348b0a497fec0c147
Eric Laquer <LaquerEric@gmail.com>
2015-02-18 16:02:00 -0500

    removed half-baked stuff

--- app/controllers/page/all_controller.rb
+++ app/controllers/page/all_controller.rb
@@ -16,33 +16,11 @@ module Page
       @digest_view_template = "digest-rails/shared/digest"
     end
 
-    def add_layout_rendering(pane_rendering)
-      {
-          navigation: 'Nav from layout'
-      }
-    end
-
-    def render_for_main_pane(main_pane_rendering)
-      main_pane_rendering.merge!(
-          add_layout_rendering(main_pane_rendering)
-      )
-    end
-
     def name_server
       render :text => "Route:#{get_route}"
     end
 
     private
-
-=begin
-    def navigation
-      @navigation = "NN from Layout Controller"
-    end
-
-    def main_pane
-      @main_pane = "PP from Controller"
-    end
-=end
 
   end
 end
--- app/controllers/pane/main_controller.rb
+++ app/controllers/pane/main_controller.rb
@@ -2,18 +2,5 @@ require_dependency "page/all_controller"
 
 module Pane
   class MainController < Page::AllController
-
-      def add_main_pane_rendering(pane_content)
-        {
-            main_pane: "MainPPPane"#get_partial(:main_pane,pane_content)
-        }
-      end
-
-      def render_for_content(pane_content)
-        render_for_main_pane(
-            add_main_pane_rendering(pane_content)
-        )
-      end
-
   end
 end
