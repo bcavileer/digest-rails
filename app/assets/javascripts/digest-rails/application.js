9fe500ae48482b510889e31107514b3c48229c5d
Eric Laquer <LaquerEric@gmail.com>
2015-02-19 20:31:53 -0500

    Tab to digest works

--- app/assets/javascripts/digest-rails/application.js
+++ app/assets/javascripts/digest-rails/application.js
@@ -9,13 +9,11 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery.js
 //= require jquery_ujs
 //= require foundation
 //= require handsontable_dr
 //= require axle
-
 
 
 $(function(){ $(document).foundation(); });
