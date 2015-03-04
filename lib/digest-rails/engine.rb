7aad0680534857d28dcd8c4e962cbee02c4ceee5
Eric Laquer <LaquerEric@gmail.com>
2015-03-03 22:08:35 -0500

    Last of browserify

--- lib/digest-rails/engine.rb
+++ lib/digest-rails/engine.rb
@@ -7,19 +7,10 @@ module DigestRails
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
       g.assets false
       g.helper false
-
     end
-
-    #config.browserify_rails.paths << lambda { |p| p.start_with?(Engine.root.join("app").to_s) }
 
     def eager_load!
     end
-
-=begin
-    config.eager_load_paths = config.eager_load_paths.select do |p|
-      p.split('/')[-2..-1].join('/') != 'app/assets'
-    end
-=end
 
   end
 end
