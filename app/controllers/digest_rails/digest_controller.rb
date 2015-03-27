b347743b3ef8c8e73b7be37fd3ba6e4256c1ec62
Eric Laquer <LaquerEric@gmail.com>
2015-03-27 13:56:47 -0400

    Serviewer handles client-side except js

--- app/controllers/digest_rails/digest_controller.rb
+++ app/controllers/digest_rails/digest_controller.rb
@@ -5,22 +5,25 @@ module DigestRails
   class DigestController < Pane::MainController
     before_filter :prepare_digest
 
-    def prepare_digest
+    def stylesheet_link_tag(regular, hash={})
+      self.view_context.stylesheet_link_tag(regular, hash)
+    end
 
+    def javascript_include_tag(regular, hash={})
+      self.view_context.javascript_include_tag(regular, hash)
+    end
+
+    def prepare_digest
       @active_digest_name = self.class.to_s.split('::').last.underscore.split('_')[0..-2].join('_')
       @active_digest = @digests_crosses[:digests].select{ |d| d.key.underscore == @active_digest_name }.first
       @active_digest_index = @active_digest.instance_variable_get(:@index)
-
     end
 
     def json
-
       render json: @digests_crosses[:digests][@active_digest_index]
-
     end
 
     def show_response
-
       endpoint = @active_digest.data_view.core_set.endpoint
       model_name = @active_digest.data_view.core_set.item_hash.key.to_s.split('/')[1..-1].join('_')
       digest_json_url = get_route(engine: endpoint.engine, route_name: [@active_digest_name,'json'].join('_') )
@@ -33,7 +36,6 @@ module DigestRails
           digest_json_url: digest_json_url,
           core_set_html_url: core_set_html_url
       )
-
     end
 
     def active_digest_index
@@ -45,10 +47,8 @@ module DigestRails
     end
 
     def show
-
       render :text => Template['digest-rails/layout/base'].render(self)
       #js 'Digest#show',show_response
-
     end
 
   end
--- lib/digest-rails/serviewer_environment.rb
+++ lib/digest-rails/serviewer_environment.rb
@@ -3,9 +3,12 @@ require 'rubygems'
 require 'bundler/setup'
 require 'fileutils'
 require 'open3'
-
 module Serviewer
-
+  class Engine
+    def self.config(c)
+      yield c
+    end
+  end
 
   ############################
   # Each File
@@ -50,7 +53,6 @@ module Serviewer
           line_number += 1
           r
         end.compact
-
     end
 
     def report_javascript_lines
@@ -165,13 +167,11 @@ module Serviewer
   end
 
   module ProcessCodeFile
-
     def process_file
       process_raw_file
       javascript_lines
       init_dependancies
     end
-
   end
 
   module AnySourceFile
@@ -214,20 +214,14 @@ module Serviewer
       @library_type = fileset_class.library_types.select do |library_type|
         source_path_split.include? library_type.name
       end.first
+
       return self
     end
 
   end
 
-  module ProcessTemplateFile
-    def output_path
-      File.join(fileset_class.output_subdir,@library_name,@template_dir,output_filename)
-    end
-
-    def output_filename
-      [@name,fileset_class.output_template_ext].join('.')
-    end
-
+  ## For File
+  module OutputTemplate
     def write_output_file
       p "  Writing Template to #{output_path}"
       FileUtils::mkdir_p File.dirname(output_path)
@@ -239,34 +233,72 @@ module Serviewer
       end
     end
 
+    private
+
+    def output_path
+      File.join(fileset_class.output_subdir,@library_name,@template_dir,output_filename)
+    end
+
+    def output_filename
+      [@name,fileset_class.output_template_ext].join('.')
+    end
+
+  end
+
+  ## For File
+  module ProcessTemplateFile
     def process_template
       process_raw_file
       write_output_file
     end
   end
 
-  class SourceTemplateFile
-    include AnySourceFile
-    include ProcessTemplateFile
-    attr_accessor :extension, :library_name, :library_type, :path, :key
-  end
+  ## For File
+  module SourceTemplate
 
-  class SourceCodeFile
-    include AnySourceFile
-    attr_accessor :extension, :library_name, :library_type, :path, :key
-
-    include GetJavascriptLines
-    include Dependancies
-    include AltLibrary
-    include ProcessCodeFile
+    def self.included(base)
+      base.send :include, ProcessTemplateFile
+      base.send :include, OutputTemplate
+    end
 
   end
 
-  ############################
+  ## For File
+  module  SourceCode
+    def self.included(base)
+      base.send :include, GetJavascriptLines
+      base.send :include, Dependancies
+      base.send :include, AltLibrary
+      base.send :include, ProcessCodeFile
+    end
+  end
+
+  class SourceFile
+    include AnySourceFile
+    attr_accessor :fileset_class
+    attr_accessor :extension, :library_name, :library_type, :path, :key
+
+    def filename
+      "#{key}.#{extension.name}"
+    end
+
+  end
+
+  class SourceTemplateFile < SourceFile
+    include SourceTemplate
+  end
+
+  class SourceCodeFile < SourceFile
+    include SourceCode
+  end
+
+  #######################################################
   # All Files
-  ############################
+  #######################################################
 
+  ## For File Set
   module SourceFiles
+
     attr_reader :extensions, :library_types, :files
 
     def files
@@ -322,6 +354,7 @@ module Serviewer
 
   end
 
+  ## For Server File Set
   module RunThruRuby
 
     def run_thru_ruby
@@ -351,6 +384,7 @@ module Serviewer
 
   end
 
+  ## For File Set
   module DependancySchedule
 
     def files_by_schedule
@@ -361,7 +395,15 @@ module Serviewer
       end
     end
 
+    def map_dependancies
+      p "Mapping Dependancies..."
+      files.each do |file|
+        file.map_dependancies(files_hash)
+      end
+    end
+
     def schedule_dependancies
+      p "Scheduling Dependancies..."
       (1 .. files.length).each do |iteration|
         scheduled = schedule_dependancy_iteration(iteration)
         p scheduled.length
@@ -390,6 +432,7 @@ module Serviewer
 
   end
 
+  ## For File Set
   module ProcessTemplateFiles
 
     def process_files
@@ -410,15 +453,16 @@ module Serviewer
 
   end
 
+  ## For File Set
   module ProcessCodeFiles
 
     def process_files
       print_seperator_1
-      p "Running #{self.class} to create file: #{self.output_path}"
+      process_code_file_description
       print_seperator_2
 
       p "Removing old files..."
-      FileUtils.remove_dir(self.output_subdir,true)
+      FileUtils.remove_dir(self.code_output_subdir,true)
       print_seperator_2
 
       p "Processing Code Files..."
@@ -426,27 +470,29 @@ module Serviewer
         file.process
       end
 
-      p "Mapping Dependancies..."
-      files.each do |file|
-        file.map_dependancies(files_hash)
-      end
-
-      p "Scheduling Dependancies..."
+      map_dependancies
       schedule_dependancies
 
       p "Report all..."
       report_all
 
-      p "Write code file..."
-      write_code_file
+      write_code_files
 
       #p "Run Thru Ruby..."
       #run_thru_ruby
+      print_seperator_2
     end
 
   end
 
-  module OutputCode
+  ## For File Set
+  module OutputClientCode
+    def report_all
+    end
+  end
+
+  ## For File Set
+  module OutputServerCode
 
     def report_all
 
@@ -474,10 +520,7 @@ module Serviewer
 
   end
 
-  module OutputTemplates
-
-  end
-
+  ## For File Set
   module All
 
     def self.included(base)
@@ -501,6 +544,7 @@ module Serviewer
   end
   ############################
 
+  ## For File Set
   module ServerOrClient
     def root_dir
       if rails_config_init_file_path
@@ -509,9 +553,9 @@ module Serviewer
         File.join(File.dirname(__FILE__),'..')
       end
     end
-
   end
 
+  ## For File Set
   module Server
 
     def self.included(base)
@@ -520,6 +564,26 @@ module Serviewer
       base.send :include, DependancySchedule
       base.send :include, ProcessCodeFiles
       base.send :include, RunThruRuby
+      base.send :include, OutputServerCode
+    end
+
+    def process_code_file_description
+      p "Running #{self.class} to create single server code file in: #{self.code_output_path}"
+    end
+
+    def output_template_ext
+      'erb'
+    end
+
+    def code_output_path
+      File.join(code_output_subdir,output_filename)
+    end
+
+    private
+
+    def write_code_files
+      p "Writing Server code file..."
+      write_code_file
     end
 
     def output_dir
@@ -528,101 +592,10 @@ module Serviewer
       )
     end
 
-    def output_template_ext
-      'erb'
-    end
-
-  end
-
-  module Client
-
-    def output_dir
-      File.expand_path(
-          File.join(root_dir,'app/assets/javascripts/serviewer')
-      )
-    end
-
-    def output_template_ext
-      'opalerb'
-    end
-
-  end
-
-  ############################
-
-  module CodeOrTemplates
-
-    def all_library_types
-      %w{ poly_lib opal_lib ruby_lib js_lib  views_lib }.map{ |n| t = LibraryType.new; t.name = n; t }
-    end
-
-    def all_extensions
-      %w{ rb js.opal js.opalerb js js.es6 html.opalerb }.map{ |n| e = Extension.new; e.name = n; e }
-    end
-
-    def get_library_types(type_names)
-      all_library_types.select do |library_type|
-        type_names.include? library_type.name
-      end
-    end
-
-    def get_extensions(extension_names)
-      extension_names
-      all_extensions.select do |extension|
-        extension_names.include? extension.name
-      end
-    end
-
-    def source_file_new
-      SourceTemplateFile.new
-    end
-
-    def glob_search_base(load_path)
-      File.expand_path(
-          File.join( load_path, '..','**', 'app', 'views' )
-      )
-    end
-
-  end
-
-  module Code
-
-    def self.included(base)
-      base.send :include, ProcessCodeFiles
-      base.send :include, OutputCode
-    end
-
-    def path_labels(source_path_split)
-      {
-        library_name: source_path_split[-3],
-        library_type: source_path_split[-2]
-      }
-    end
-
-    def library_types
-      get_library_types( %w{ poly_lib ruby_lib } )
-    end
-
-    def extensions
-      get_extensions( %w{ rb js.opal } )
-    end
-
-    def new_source_file
-      SourceCodeFile.new
-    end
-
-    def output_subdir
-      File.join(output_dir,'code')
-    end
-
-    def output_path
-      File.join(output_subdir,output_filename)
-    end
-
     def write_code_file
-      p output_path
-      FileUtils::mkdir_p File.dirname(output_path)
-      File.open(output_path,'w') do |f|
+      p code_output_path
+      FileUtils::mkdir_p File.dirname(code_output_path)
+      File.open(code_output_path,'w') do |f|
         content_lines.each do |l|
           f.puts l
         end
@@ -646,6 +619,136 @@ module Serviewer
       end.flatten
     end
 
+  end
+
+  ## For File Set
+  module Client
+
+    def self.included(base)
+      base.send :include, OutputClientCode
+    end
+
+    def process_code_file_description
+      p "Running #{self.class} to create code file tree under: #{code_output_subdir}"
+    end
+
+    def map_dependancies
+    end
+
+    def schedule_dependancies
+    end
+
+    def output_dir
+      File.expand_path(
+          File.join(root_dir,'app/assets/javascripts/serviewer')
+      )
+    end
+
+    def code_output_path(file)
+      File.join(code_output_subdir,file.filename)
+    end
+
+    def output_template_ext
+      'opalerb'
+    end
+
+    def write_code_files
+      files.each do |file|
+        p "Writing Client code file #{code_output_path(file)}..."
+        FileUtils::mkdir_p File.dirname(code_output_path(file))
+        File.open(code_output_path(file),'w') do |f|
+          file.content_lines.each do |l|
+            f.puts l
+          end
+        end
+
+        #p code_output_path(file)
+        #write_code_file(file)
+        p
+      end
+
+    end
+
+    def write_code_file(file)
+      p code_output_path(file)
+      #FileUtils::mkdir_p File.dirname(code_output_path)
+      #File.open(code_output_path,'w') do |f|
+      #  content_lines.each do |l|
+      #    f.puts l
+      #  end
+      #end
+    end
+
+  end
+
+  ############################
+
+  ## For File Set
+  module CodeOrTemplates
+
+    def all_library_types
+      %w{ poly_lib opal_lib ruby_lib js_lib  views_lib }.map{ |n| t = LibraryType.new; t.name = n; t }
+    end
+
+    def all_extensions
+      %w{ rb js.opal js.opalerb js js.es6 html.opalerb }.map{ |n| e = Extension.new; e.name = n; e }
+    end
+
+    def get_library_types(type_names)
+      all_library_types.select do |library_type|
+        @library_type_names.include? library_type.name
+      end
+    end
+
+    def get_extensions(extension_names)
+      extension_names
+      all_extensions.select do |extension|
+        @extension_names.include? extension.name
+      end
+    end
+
+    def source_file_new
+      SourceTemplateFile.new
+    end
+
+    def glob_search_base(load_path)
+      File.expand_path(
+          File.join( load_path, '..','**', 'app', 'views' )
+      )
+    end
+
+  end
+
+  ## For File Set
+  module Code
+
+    def self.included(base)
+      base.send :include, ProcessCodeFiles
+    end
+
+    def code_output_subdir
+      File.join(output_dir,'code')
+    end
+
+    def path_labels(source_path_split)
+      {
+        library_name: source_path_split[-3],
+        library_type: source_path_split[-2]
+      }
+    end
+
+    def library_types
+      get_library_types( %w{ poly_lib ruby_lib } )
+    end
+
+    def extensions
+      get_extensions( %w{ rb js.opal } )
+    end
+
+    def new_source_file
+      SourceCodeFile.new
+    end
+
     def glob_search(load_path)
       File.join( glob_search_base(load_path), library_type_search, extension_search )
     end
@@ -656,7 +759,6 @@ module Serviewer
 
     def self.included(base)
       base.send :include, ProcessTemplateFiles
-      base.send :include, OutputTemplates
     end
 
     def path_labels(source_path_split)
@@ -692,7 +794,7 @@ module Serviewer
 
   ############################
 
-  class ServiewerBase
+  class Base
     attr_accessor :rails_config_init_file_path
 
     def initialize(c)
@@ -702,7 +804,7 @@ module Serviewer
 
   ############################
 
-  class ServiewerServerCode < ServiewerBase
+  class ServerCode < Base
     include All
     include ServerOrClient
     include Server
@@ -717,22 +819,32 @@ module Serviewer
       'index.rb'
     end
 
+    def initialize(c)
+      super
+      @library_type_names = %w{ poly_lib opal_lib ruby_lib js_lib  views_lib }
+      @extension_names = %w{ rb js.opal }
+    end
+
   end
-  #Serviewer::ServiewerServerCode.new.process_files
 
-
-  class ServiewerServerTemplates < ServiewerBase
+  class ServerTemplates < Base
     include All
     include ServerOrClient
     include Server
     include CodeOrTemplates
     include Templates
+
+    def initialize(c)
+      super
+      @library_type_names = %w{ view_lib }
+      @extension_names = %w{ js.opalerb }
+    end
+
   end
-  #Serviewer::ServiewerServerTemplates.new.process_files
 
   ############################
 
-  class ServiewerClientCode < ServiewerBase
+  class ClientCode < Base
     include All
     include ServerOrClient
     include Client
@@ -741,8 +853,8 @@ module Serviewer
 
     def initialize(c)
       super
-      get_library_types( %w{ views_lib } )
-      get_extensions( %w{ js.opalerb } )
+      @library_type_names = %w{ poly_lib opal_lib js_lib }
+      @extension_names = %w{ js.opal js.es6 }
     end
 
     def output_filname
@@ -750,9 +862,8 @@ module Serviewer
     end
 
   end
-  #ServiewerClientCode.new.process_files
 
-  class ServiewerClientTemplates < ServiewerBase
+  class ClientTemplates < Base
     include All
     include ServerOrClient
     include Client
@@ -761,8 +872,8 @@ module Serviewer
 
     def initialize(c)
       super
-      get_library_types( %w{ poly_lib opal_lib js_lib } )
-      get_extensions( %w{ rb js.opal js.opalerb } )
+      @library_type_names =  %w{ views_lib }
+      @extension_names = %w{ js.opalerb }
     end
 
     def output_filname
@@ -770,14 +881,6 @@ module Serviewer
     end
 
   end
-  #ServiewerClientTemplates.new.process_files
 
-############################
 end
-
-#Serviewer::ServiewerServerTemplates.new.process_files
-#Serviewer::ServiewerServerCode.new.process_files
-
-
-
 
